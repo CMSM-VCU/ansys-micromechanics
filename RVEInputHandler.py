66bda8c3d371b9abcd8a231083a28e0b0db0d324
Riley Hall <hallrc@vcu.edu>
2020-09-01 17:28:20 -0400

    Add path to list of required properties

--- RVEInputHandler.py
+++ RVEInputHandler.py
@@ -9,6 +9,6 @@ class RVEInputHandler(InputHandler):
     def create_testcase_class(self):
         return RecursiveClassFactory.create_class(
             "RVETestCase",
-            required_args=self.get_required_properties(),
+            required_args=self.get_required_properties() + ["path"],
             BaseClass=TestCaseSkeleton,
         )
