b38326bb41cf7846107431afb5b121a358b0d9a7
Riley Hall <hallrc@vcu.edu>
2020-09-01 15:28:54 -0400

    Extract AnsysContainer class from TestRunner

---
+++ AnsysContainer.py
@@ -0,0 +1,23 @@
+import pyansys
+
+
+class AnsysContainer:
+    def __init__(self, launch_options=None):
+        self.launch_options = launch_options
+
+    def __enter__(self):
+        # TODO: Add exception handling for lock file conflict
+        if isinstance(self.launch_options, dict):
+            self.ansys = pyansys.launch_mapdl(**self.launch_options)
+        else:
+            self.ansys = pyansys.launch_mapdl()
+
+        setattr(self.ansys, "pyansys_version", pyansys.__version__)
+        return self.ansys
+
+    def __exit__(self, type, value, traceback):
+        """Ensure that Ansys instance is closed when done using class.
+        """
+        print("Attempting to exit Ansys...")
+        self.ansys.exit()
+        print("Exited successfully")
