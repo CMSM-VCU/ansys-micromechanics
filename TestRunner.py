5b08a783e67516af7c968794e5e765c1baa14482
Riley Hall <hallrc@vcu.edu>
2020-09-01 15:47:27 -0400

    Convert for TestCase attachment and AnsysContainer

--- TestRunner.py
+++ TestRunner.py
@@ -1,9 +1,9 @@
-from typing import List, Any
-import numpy as np
-import pyansys
+from typing import Any, List
 
-from utils import logger_wraps, decorate_all_methods
-from tuples import Material
+import numpy as np
+
+from AnsysContainer import AnsysContainer
+from utils import decorate_all_methods, logger_wraps
 
 AXES = ["X", "Y", "Z"]
 
@@ -22,60 +22,45 @@ DECIMAL_PLACES = 5
 @decorate_all_methods(logger_wraps)
 class TestRunner:
     ansys: Any  #: pyansys.mapdl_corba.MapdlCorba # don't know how to type hint this
-    element_type: int
     launch_options: dict
     retained_nodes: List
 
-    def __init__(self, element_type=185, launch_options=None):
+    def __init__(self, test_case, options=None):
         """Launches an Ansys instance. See pyansys documentation for launch options such
         as job directory and executable path.
 
         Args:
             launch_options (dict, optional): dictionary of keyword arguments for pyansys.launch_mapdl()
         """
-        self.element_type = element_type
-        self.launch_options = launch_options
+        self.test_case = test_case
+        self.launch_options = options
 
-    def __enter__(self):
-        # TODO: Add exception handling for lock file conflict
-        if isinstance(self.launch_options, dict):
-            self.ansys = pyansys.launch_mapdl(**self.launch_options)
-        else:
-            self.ansys = pyansys.launch_mapdl()
+    def run(self):
+        with AnsysContainer(self.launch_options) as self.ansys:
+            self.prepare_mesh()
+            self.run_test_sequence()
 
-        self.ansys.finish()
-        self.ansys.run("/CLEAR")
+    def prepare_mesh(self):
+        self.generate_base_mesh()
+        self.get_retained_nodes()
+        self.define_materials()
+        self.assign_element_materials()
+        self.apply_periodic_conditions()
 
-        return self
-
-    def __exit__(self, type, value, traceback):
-        """Ensure that Ansys instance is closed when done using class.
-        """
-        print("Attempting to exit Ansys...")
-        self.ansys.exit()
-        print("Exited successfully")
-        # Add check to see if closed successfully?
-
-    def prepare_mesh(self, dimensions, materials, arrangement):
-        self.generate_base_mesh(dimensions)
-        self.get_retained_nodes(dimensions)
-        self.define_materials(materials)
-        self.assign_element_materials(arrangement)
-        self.apply_periodic_conditions(dimensions)
-
-    def run_test_sequence(self, loads):
+    def run_test_sequence(self):
         for load_case in range(1, 7):
             self.ansys.ddele("ALL")  # Will need to change for any other loading methods
             if load_case < 4:
-                self.apply_loading_normal(load_case, loads)
+                self.apply_loading_normal(load_case)
             elif load_case >= 4:
-                self.apply_loading_shear(load_case - 3, loads)
+                self.apply_loading_shear(load_case - 3)
 
             self.solve()
             self.debug_stat()
             self.extract_raw_results()
+            self.calculate_properties()
 
-    def generate_base_mesh(self, dimensions):
+    def generate_base_mesh(self):
         """Generate uniform cubic mesh according to overall side length and element edge
         length. Assumes a cube centered around (0,0,0).
 
@@ -83,20 +68,20 @@ class TestRunner:
             dimensions (Dims): tuple containing side length and element length
         """
         print("generate_base_mesh")
-        half_side = dimensions.side_length / 2
+        half_side = self.test_case.mesh.domainSideLength / 2
         self.ansys.run("/PREP7")
         self.ansys.block(
             -half_side, half_side, -half_side, half_side, -half_side, half_side,
         )
 
-        self.ansys.et(1, self.element_type)
-        self.ansys.lesize("ALL", dimensions.element_length)
+        self.ansys.et(1, self.test_case.mesh.elementType)
+        self.ansys.lesize("ALL", self.test_case.mesh.elementSpacing)
         self.ansys.mshkey(1)
         self.ansys.vmesh("ALL")
 
-    def get_retained_nodes(self, dimensions):
+    def get_retained_nodes(self):
         coord_signs = [[-1, -1, -1], [1, -1, -1], [-1, 1, -1], [-1, -1, 1]]
-        magnitude = dimensions.side_length / 2
+        magnitude = self.test_case.mesh.domainSideLength / 2
 
         node_coords = [[sign * magnitude for sign in node] for node in coord_signs]
 
@@ -112,7 +97,7 @@ class TestRunner:
         # node is CLOSEST to the location.
         inline = f"node({x},{y},{z})"
         self.ansys.run("NODE_NUMBER_TEMP=" + inline)
-        if pyansys.__version__[:4] == "0.43":
+        if self.ansys.pyansys_version[:4] == "0.43":
             return int(self.ansys.parameters["node_number_temp"])
         else:
             self.load_parameters()
@@ -123,26 +108,26 @@ class TestRunner:
         if kind:
             self.ansys.nsel(kind, "NODE", "", nnum)
 
-    def define_materials(self, materials):
+    def define_materials(self):
         self.ansys.run("/PREP7")
         e_str = ["EX", "EY", "EZ"]
         g_str = ["GXY", "GYZ", "GXZ"]
         pr_str = ["PRXY", "PRYZ", "PRXZ"]
-        for material in materials:
-            id = material.id
+        for material in self.test_case.materials:
+            id = material.materialIndex
             for i in range(3):
-                self.ansys.mp(e_str[i], id, material.elastic_moduli[i])
-                self.ansys.mp(g_str[i], id, material.shear_moduli[i])
-                self.ansys.mp(pr_str[i], id, material.poisson_ratios[i])
+                self.ansys.mp(e_str[i], id, material.elasticModuli[i])
+                self.ansys.mp(g_str[i], id, material.shearModuli[i])
+                self.ansys.mp(pr_str[i], id, material.poissonsRatios[i])
         # How do I verify that materials were input correctly? How do I access the
         # materials from self.ansys?
 
-    def assign_element_materials(self, arrangement):
+    def assign_element_materials(self):
         # This implementation is probably super slow
         # Individual emodif commands may be extra slow, so try adding to component
         # per material number, then emodif on each component
         self.ansys.run("/PREP7")
-        for element in arrangement:
+        for element in self.test_case.mesh.locationsWithId:
             self.ansys.esel("S", "CENT", "X", element[0])
             self.ansys.esel("R", "CENT", "Y", element[1])
             self.ansys.esel("R", "CENT", "Z", element[2])
@@ -150,14 +135,14 @@ class TestRunner:
 
         self.ansys.allsel()
 
-    def apply_periodic_conditions(self, dimensions):
+    def apply_periodic_conditions(self):
         self.ansys.run("/PREP7")
 
-        pair_sets = self.find_node_pairs(dimensions)
+        pair_sets = self.find_node_pairs()
 
         rn = self.retained_nodes
 
-        if pyansys.__version__[:4] == "0.43":
+        if self.ansys.pyansys_version[:4] == "0.43":
             context = self.ansys.chain_commands
         else:
             context = self.ansys.non_interactive
@@ -187,22 +172,22 @@ class TestRunner:
                         # fmt: on
         pass
 
-    def find_node_pairs(self, dimensions):
+    def find_node_pairs(self):
         pair_sets = []
 
         for i, axis in enumerate(AXES):  # Select exterior nodes on each axis
-            self.ansys.nsel("S", "LOC", axis, dimensions.side_length / 2)
+            self.ansys.nsel("S", "LOC", axis, self.test_case.mesh.domainSideLength / 2)
 
-            if pyansys.__version__[:4] == "0.43":
+            if self.ansys.pyansys_version[:4] == "0.43":
                 nodes_pos = np.around(self.ansys.mesh.nodes, DECIMAL_PLACES)
                 nnum_pos = self.ansys.mesh.nnum
             else:
                 nodes_pos = np.around(self.ansys.nodes, DECIMAL_PLACES)
                 nnum_pos = self.ansys.nnum
 
-            self.ansys.nsel("S", "LOC", axis, -dimensions.side_length / 2)
+            self.ansys.nsel("S", "LOC", axis, -self.test_case.mesh.domainSideLength / 2)
 
-            if pyansys.__version__[:4] == "0.43":
+            if self.ansys.pyansys_version[:4] == "0.43":
                 nodes_neg = np.around(self.ansys.mesh.nodes, DECIMAL_PLACES)
                 nnum_neg = self.ansys.mesh.nnum
             else:
@@ -238,22 +223,26 @@ class TestRunner:
 
         return pair_sets
 
-    def apply_loading_normal(self, axis, loads):
+    def apply_loading_normal(self, axis):
         for j, n in enumerate(self.retained_nodes):
             for axis_label in NORMAL_FIXED_AXES[j]:
                 self.ansys.d(n, axis_label)
 
         self.ansys.d(
-            self.retained_nodes[axis], "U" + AXES[axis - 1], loads.normal_magnitude
+            self.retained_nodes[axis],
+            "U" + AXES[axis - 1],
+            self.test_case.loading.normalMagnitude,
         )
 
-    def apply_loading_shear(self, axis, loads):
+    def apply_loading_shear(self, axis):
         for j, n in enumerate(self.retained_nodes):
             for axis_label in SHEAR_FIXED_AXES[j]:
                 self.ansys.d(n, axis_label)
 
         self.ansys.d(
-            self.retained_nodes[axis], "U" + AXES[axis % 3], loads.shear_magnitude
+            self.retained_nodes[axis],
+            "U" + AXES[axis % 3],
+            self.test_case.loading.shearMagnitude,
         )
 
         # How do I verify that load steps were input correctly? How do I access the
@@ -269,8 +258,7 @@ class TestRunner:
         pass
 
     def calculate_properties(self):
-        properties = Material(99, [1], [1], [1])
-        return properties
+        self.test_case.results = {"dummy": 999}
 
     def load_parameters(self):
         try:
