35b930b467f69b7789d33d42f4c2fde4bd59ed7d
Riley Hall <hallrc@vcu.edu>
2020-09-08 14:33:05 -0400

    Rewrite find_node_pairs() for nodes near zero

--- PBCHandler.py
+++ PBCHandler.py
@@ -5,7 +5,8 @@ from utils import round_to_sigfigs
 from utils import decorate_all_methods, logger_wraps
 
 AXES = ["X", "Y", "Z"]
-SIG_FIGS = 10
+SIG_FIGS = 8
+EPSILON = np.finfo(float).eps * 1e3  # Should be ~1e-13
 
 
 @decorate_all_methods(logger_wraps)
@@ -44,38 +45,34 @@ class PBCHandler:
     def find_node_pairs(self):
         pair_sets = []
 
-        for i, axis in enumerate(AXES):  # Select exterior nodes on each axis
-            self.ansys.nsel("S", "LOC", axis, self.mesh_extents()[i, 1])
+        for axis_ind, axis in enumerate(AXES):  # Select exterior nodes on each axis
+            self.ansys.nsel("S", "LOC", axis, self.mesh_extents()[axis_ind, 1])
 
             nodes_pos = round_to_sigfigs(self.ansys.mesh.nodes, SIG_FIGS)
-            nnum_pos = self.ansys.mesh.nnum
+            nnum_pos = np.reshape(self.ansys.mesh.nnum, (-1, 1))
 
-            self.ansys.nsel("S", "LOC", axis, self.mesh_extents()[i, 0])
+            self.ansys.nsel("S", "LOC", axis, self.mesh_extents()[axis_ind, 0])
 
             nodes_neg = round_to_sigfigs(self.ansys.mesh.nodes, SIG_FIGS)
-            nnum_neg = self.ansys.mesh.nnum
+            nnum_neg = np.reshape(self.ansys.mesh.nnum, (-1, 1))
 
-            print(nodes_pos.shape, nodes_neg.shape)
+            assert nodes_pos.shape == nodes_neg.shape
 
-            # Delete coordinate along current axis
-            nodes_pos = np.delete(nodes_pos, i, 1)
-            nodes_neg = np.delete(nodes_neg, i, 1)
+            # Delete coordinates column for current axis
+            nodes_pos = np.delete(nodes_pos, axis_ind, axis=1)
+            nodes_neg = np.delete(nodes_neg, axis_ind, axis=1)
+
+            # Replace values near zero with exactly zero
+            nodes_pos = np.where(np.abs(nodes_pos) > EPSILON, nodes_pos, 0.0)
+            nodes_neg = np.where(np.abs(nodes_neg) > EPSILON, nodes_neg, 0.0)
 
             # Get coordinates and number in one row
-            face_nodes_pos = np.hstack(
-                (nodes_pos, np.reshape(nnum_pos, (len(nnum_pos), 1)),)
-            )
-            face_nodes_neg = np.hstack(
-                (nodes_neg, np.reshape(nnum_neg, (len(nnum_neg), 1)),)
-            )
+            face_nodes_pos = np.hstack([nodes_pos, nnum_pos])
+            face_nodes_neg = np.hstack([nodes_neg, nnum_neg])
 
             # Sort coordinates so counterparts will be at same index
-            face_nodes_pos = face_nodes_pos[
-                np.lexsort((face_nodes_pos[:, 0], face_nodes_pos[:, 1],))
-            ]
-            face_nodes_neg = face_nodes_neg[
-                np.lexsort((face_nodes_neg[:, 0], face_nodes_neg[:, 1],))
-            ]
+            face_nodes_pos = face_nodes_pos[np.lexsort(np.transpose(nodes_pos))]
+            face_nodes_neg = face_nodes_neg[np.lexsort(np.transpose(nodes_neg))]
 
             assert self.verify_equality(face_nodes_pos[:, :2], face_nodes_neg[:, :2])
 
