6177c1ef70a79f5a69135fc71abd0bbbd96b8f11
Riley Hall <hallrc@vcu.edu>
2020-09-15 17:26:43 -0400

    Implement conversion of directions to strain tensors

--- LoadingHandler.py
+++ LoadingHandler.py
@@ -1,3 +1,4 @@
+import numpy as np
 from utils import decorate_all_methods, logger_wraps
 
 AXES = ["X", "Y", "Z"]
@@ -18,6 +19,54 @@ class LoadingHandler:
         self.retained_nodes = testrunner.retained_nodes
 
         self.loading = testrunner.test_case.loading
+        self.prepare_loading(testrunner.mesh_extents())
+
+    def prepare_loading(self, extents):
+        if self.loading.kind == "displacement":
+            self.tensors = self.convert_directions_to_strain_tensors(
+                self.loading.directions,
+                self.loading.normalMagnitude,
+                self.loading.shearMagnitude,
+                extents,
+            )
+        elif self.loading.kind == "tensor":
+            self.tensors = (
+                np.array(self.loading.tensors) * self.loading.magnitudeMultiplier
+            ).tolist()
+        print(self.tensors)
+        pass
+
+    @staticmethod
+    def convert_directions_to_strain_tensors(
+        directions, normal_mag, shear_mag, extents
+    ):
+        return [
+            LoadingHandler.convert_direction_to_strain_tensor(
+                direction, normal_mag, shear_mag, extents
+            )
+            for direction in directions
+        ]
+
+    @staticmethod
+    def convert_direction_to_strain_tensor(direction, normal_mag, shear_mag, extents):
+        base = ([0, 0, 0], [0, 0, 0], [0, 0, 0])
+        if len(direction) == 3:
+            sign = -1
+        else:
+            sign = 1
+
+        i, j = list(map(lambda x: int(x) - 1, direction[-2:]))
+
+        if i == j:
+            mag = normal_mag
+        else:
+            mag = shear_mag
+
+        mag = mag / (extents[i, 1] - extents[i, 0])
+
+        tensor = list(base)
+        tensor[i][j] = sign * mag
+        return tensor
 
     def apply_loading_normal(self, axis):
         for j, n in enumerate(self.retained_nodes):
