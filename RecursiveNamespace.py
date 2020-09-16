7a1448b0660c07f686d9dbf34bb0706a73c60373
Riley Hall <hallrc@vcu.edu>
2020-09-16 18:42:11 -0400

    Add keys and __getitem__ methods to enable ** unpacking

--- RecursiveNamespace.py
+++ RecursiveNamespace.py
@@ -23,3 +23,9 @@ class RecursiveNamespace:
             )
             + "\n"
         )
+
+    def keys(self):
+        return vars(self).keys()
+
+    def __getitem__(self, key):
+        return getattr(self, key)
