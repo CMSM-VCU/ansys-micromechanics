2155ce296a0c684e3e66917e71ad93d905400ad3
Riley Hall <hallrc@vcu.edu>
2020-09-16 12:20:55 -0400

    Add status return values to InputHandler methods

--- InputHandler.py
+++ InputHandler.py
@@ -24,6 +24,12 @@ class InputHandler:
         with open(schema_file_path) as schema_file:
             self.schema = json.load(schema_file)
 
+        if self.schema:
+            return True
+        else:
+            print("InputHandler: Schema load failed")
+            return False
+
     def check_input_with_schema(self, input_dict, skip_fail=False):
         if self.schema:
             try:
@@ -36,6 +42,7 @@ class InputHandler:
                     raise err
         else:
             print("InputHandler: No schema loaded")
+            return False
 
     def load_input_files(self, input_file_paths: List, check_first=True):
         for file_path in input_file_paths:
@@ -44,6 +51,7 @@ class InputHandler:
                 input_dict["path"] = pathlib.Path(file_path).resolve(strict=True)
                 if check_first and self.check_input_with_schema(input_dict):
                     self.input_dicts.append(input_dict)
+        return len(self.input_dicts)
 
     def get_required_properties(self):
         # TODO: Make this recursive to get required properties of properties
@@ -52,3 +60,4 @@ class InputHandler:
             return self.schema["required"]
         else:
             print("InputHandler: No schema loaded")
+            return False
