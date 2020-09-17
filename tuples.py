764e938b9ff40ff5de2e6bcdc5301c80fa456724
Riley Hall <hallrc@vcu.edu>
2020-09-17 19:13:18 -0400

    Remove unused tuples.py

--- tuples.py
+++
@@ -1,7 +0,0 @@
-from collections import namedtuple
-
-Dims = namedtuple("Dims", ["side_length", "element_length"])
-Material = namedtuple(
-    "Material", ["id", "elastic_moduli", "shear_moduli", "poisson_ratios"]
-)
-Loads = namedtuple("Loads", ["kind", "normal_magnitude", "shear_magnitude"])
